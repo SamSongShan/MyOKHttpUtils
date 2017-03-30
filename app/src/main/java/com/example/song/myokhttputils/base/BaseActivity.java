6ddf26cc68f43be6e3d3fa204af2d83428e66de6
TuDou <1135520344@qq.com>
2017-03-31 00:12:53 +0800

    添加base

---
+++ app/src/main/java/com/example/song/myokhttputils/base/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.example.song.myokhttputils.base;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * 所有Activity的基类
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getViewResId());
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//手机竖屏
+        unbinder = ButterKnife.bind(this);
+        init();
+        loadData();
+    }
+
+    protected abstract int getViewResId();
+
+    protected void init(){}
+
+    protected void loadData(){}
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/song/myokhttputils/base/OnAdapterCallbackListener.java
@@ -0,0 +1,9 @@
+package com.example.song.myokhttputils.base;
+
+/**
+ * 适配器回调接口
+ */
+public interface OnAdapterCallbackListener {
+
+    void onCallback();
+}
